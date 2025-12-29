f1cc2c92356e8162c5bff56e10971b92b69ce452
zhangjing <892295834@qq.com>
2025-12-29 17:31:42 +0800

    feat(webapp): integrate ng-zorro-antd for UI components and theming

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -2,11 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(zh);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideNzI18n(zh_CN),
+    provideHttpClient()
   ]
 };
