baf244faa7fc1153391cab7c5bbaa096359b516d
zhangjing <892295834@qq.com>
2026-01-15 15:41:22 +0800

    feat: add configuration module with routing and component setup

---
+++ webapp/src/app/pages/configuration/configuration.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfigurationRoutingModule } from './configuration.routing';
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+        CommonModule,
+        ConfigurationRoutingModule
+    ]
+})
+export class ConfigurationModule { }
